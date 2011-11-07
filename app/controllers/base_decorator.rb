1e8de58dc851fb8d7fa7fad85bfbc34929dac569
humancopy <info@humancopy.net>
2011-11-07 10:21:09 +0100

    Move from decorator to engine's activate. Trying to resolve missing helper.

--- app/controllers/base_decorator.rb
+++
@@ -1,9 +0,0 @@
-Spree::BaseController.class_eval do
-  helper :locale
-  before_filter :set_locale, :if => proc { !session[:locale] }
-
-  protected
-  def set_locale
-    session[:locale] = I18n.locale = (request.preferred_language_from(enabled_locales) || request.compatible_language_from(enabled_locales) || I18n.default_locale).intern
-  end
-end
--- lib/spree_change_locale/engine.rb
+++ lib/spree_change_locale/engine.rb
@@ -17,6 +17,17 @@ module SpreeChangeLocale
       Dir.glob(File.join(File.dirname(__FILE__), "../../app/overrides/*.rb")) do |c|
         Rails.application.config.cache_classes ? require(c) : load(c)
       end
+
+      # add locale helper to Spree::BaseController
+      Spree::BaseController.class_eval do
+        helper :locale
+        before_filter :set_locale, :if => proc { !session[:locale] }
+
+        protected
+        def set_locale
+          session[:locale] = I18n.locale = (request.preferred_language_from(enabled_locales) || request.compatible_language_from(enabled_locales) || I18n.default_locale).intern
+        end
+      end
     end
 
     config.to_prepare &method(:activate).to_proc
