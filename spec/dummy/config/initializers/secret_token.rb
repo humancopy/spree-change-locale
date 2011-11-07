653e73e7e4d386b4fe8cec6f7513defb275f2a50
humancopy <info@humancopy.net>
2011-11-07 22:33:12 +0100

    Don't ignore dummy… It's needed for test to pass. :)

---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '7293606dfeaaf01c40f64a7ec3a892e30ca30a504bc52ad1c998682b77b89989ed9f06f03a604b9fca0092d3fd2d063fccbaf4975b6d59adc8a5399727a6c9ae'
---
+++ spec/dummy/config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  mount SpreeChangeLocale::Engine => "/"
+end
