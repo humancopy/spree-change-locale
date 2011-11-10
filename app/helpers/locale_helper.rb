50d82d0ee8e5660246e4e753abd015e3d96017db
humancopy <info@humancopy.net>
2011-11-10 16:23:30 +0100

    Add current_locale & current_locale? to LocaleHelper for easy locale check

--- app/helpers/locale_helper.rb
+++ app/helpers/locale_helper.rb
@@ -3,8 +3,14 @@ module LocaleHelper
   def enabled_locales; I18n.enabled_locales end
   def locale_enabled?(locale); I18n.locale_enabled?(locale) end
 
-  def link_to_locale(locale, name = nil)
-    link_to name || locale_name(locale), "/#{locale}#{request && request.path}" if locale_enabled?(locale)
+  def current_locale
+    session[:locale]
+  end
+  def current_locale?(locale)
+    locale == current_locale
+  end
+  def link_to_locale(locale, name = nil, *args)
+    link_to name || locale_name(locale), "/#{locale}#{request && request.path}", *args if locale_enabled?(locale)
   end
   def locale_name(locale)
     I18n.t(:this_file_language, :locale => locale) if locale_enabled?(locale)
--- spec/helpers/locale_helper_spec.rb
+++ spec/helpers/locale_helper_spec.rb
@@ -11,16 +11,42 @@ require 'spec_helper'
 #   end
 # end
 describe LocaleHelper do
+  # test locale_name
   it "should return Spanish" do
     locale_name(:es).should == 'Spanish'
   end
   it "should not return Russian" do
     locale_name(:ru).should_not == 'Russian'
   end
+
+  # test link_to_locale
   it "should link to Spanish locale" do
     link_to_locale(:es).should == '<a href="/es">Spanish</a>'
   end
+  it "should link to Spanish locale with custom name" do
+    link_to_locale(:es, 'Spanish (ES)').should == '<a href="/es">Spanish (ES)</a>'
+  end
   it "should not link to Russian locale" do
     link_to_locale(:ru).should_not == '<a href="/ru">translation missing: ru.this_file_language</a>'
+  end
+
+  # test current_locale
+  it "should return :es" do
+    session[:locale] = :es
+    current_locale.should == :es
+  end
+  it "should return not return :en" do
+    session[:locale] = :es
+    current_locale.should_not == :en
+  end
+
+  # test current_locale?
+  it "should return true" do
+    session[:locale] = :es
+    current_locale?(:es).should be_true
+  end
+  it "should return false" do
+    session[:locale] = :es
+    current_locale?(:en).should be_false
   end
 end
