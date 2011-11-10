d4ede630d4d7d91286f853f81f68c17eac432628
humancopy <info@humancopy.net>
2011-11-10 18:24:55 +0100

    Tiny change to helper.

--- app/helpers/locale_helper.rb
+++ app/helpers/locale_helper.rb
@@ -10,7 +10,7 @@ module LocaleHelper
     locale == current_locale
   end
   def link_to_locale(locale, name = nil, *args)
-    link_to name || locale_name(locale), "/#{locale}#{request && request.path}", *args if locale_enabled?(locale)
+    link_to(name || locale_name(locale), "/#{locale}#{request && request.path}", *args) if locale_enabled?(locale)
   end
   def locale_name(locale)
     I18n.t(:this_file_language, :locale => locale) if locale_enabled?(locale)
